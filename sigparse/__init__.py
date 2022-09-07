7ec6fbb7fd5fca097cbb6e2a73c76ee3587837e0
Lunarmagpie <bambolambo0@gmail.com>
2022-09-07 17:08:23 -0400

    add files

---
+++ sigparse/__init__.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+
+import dataclasses
+import typing
+import sys
+import inspect
+import forbiddenfruit
+
+
+def _PEP604():
+    """
+    Allow writing union types as X | Y
+    """
+    def _union_or(left, right):
+        return typing.Union[left, right]
+
+    setattr(typing._GenericAlias, "__or__", _union_or)
+    setattr(typing._GenericAlias, "__ror__", _union_or)
+
+    forbiddenfruit.curse(type, "__or__", _union_or)
+
+
+if sys.version_info < (3, 10):
+    _PEP604()
+
+
+@dataclasses.dataclass
+class Parameter:
+    name: str
+    annotation: typing.Any
+    default: str
+    kind: inspect._ParameterKind
+
+
+def convert_signiture(param: Parameter, type_hints: dict[str, type[typing.Any]]) -> Parameter:
+    annotation = type_hints.get(param.name)
+    return Parameter(
+        name=param.name,
+        annotation=annotation or param.annotation,
+        default=param.default,
+        kind=param.kind,
+    )
+
+
+def sigparse(func: typing.Callable[..., typing.Any]) -> typing.Sequence[Parameter]:
+    if sys.version_info >= (3, 10):
+        return inspect.signature(func, eval_str=True).parameters.values()
+
+    localns: dict[str, typing.Any] = {
+        "list": typing.List, "type": typing.Type, "dict": typing.Dict, "tuple": typing.Tuple}
+
+    if sys.version_info >= (3, 9):
+        type_hints: dict[str, typing.Any] = typing.get_type_hints(
+            func, include_extras=True, localns=localns
+        )
+    else:
+        type_hints = typing.get_type_hints(func, localns=localns)
+
+    sig = inspect.signature(func)
+
+    return [convert_signiture(param, type_hints) for param in sig.parameters.values()]
