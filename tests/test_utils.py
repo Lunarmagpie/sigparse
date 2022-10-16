49bf4d9d07d4a5adca7805c178905548ccc86333
Lunarmagpie <bambolambo0@gmail.com>
2022-10-16 03:42:15 -0400

    add tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,4 +5,8 @@ def test_unwrap():
     assert sigparse.utils.unwrap(typing.Union[int, None]) == int
     assert sigparse.utils.unwrap(typing.Union[None, None]) == None
     assert sigparse.utils.unwrap(typing.Union[int, str]) == typing.Union[int, str]
+
     assert sigparse.utils.unwrap(typing.Optional[int]) == int
+    assert sigparse.utils.unwrap(typing.Optional[None]) == None
+
+    assert sigparse.utils.unwrap(int) == int
